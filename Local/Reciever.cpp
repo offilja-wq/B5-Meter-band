d7693e93052a129e57a53bd7d5ec95412c0bf983
Nixon <nixonsp2003@gmail.com>
2026-03-27 12:15:56 +0100

    feat: display code

---
+++ Local/Reciever.cpp
@@ -0,0 +1,229 @@
+#include <MCUFRIEND_kbv.h>
+#include <Adafruit_GFX.h>
+#include <SoftwareSerial.h>
+
+typedef struct 
+{
+  uint16_t NTC_RAW_DATA;
+  uint16_t PRESSURE_RAW_DATA;
+  uint16_t HEARTBEAT_RAW_DATA;
+  uint16_t SATURATION_RAW_DATA;
+} InputData;
+
+InputData data;
+#define START_BYTE 0xAA
+SoftwareSerial mySerial(11, 12); // RX, TX
+
+MCUFRIEND_kbv tft;
+
+// Kleuren
+#define ZWART     0x0000
+#define WIT       0xFFFF
+#define ROOD      0xF800
+#define GROEN     0x07E0
+#define BLAUW     0x001F
+#define GRIJS     0x8410
+#define DGGRIJS   0x2104
+#define ROZE      0xF813
+#define LICHTROZE 0xFF18
+
+// Grafiek instellingen
+#define GRAFIEK_X   35
+#define GRAFIEK_Y   20
+#define GRAFIEK_B   130
+#define GRAFIEK_H   200
+
+#define SCHERM_B    240
+#define SCHERM_H    320
+
+// Potmeter
+//#define POTMETER_PIN A6
+//float potWaarde = 0.0;
+
+// Sinus variabelen
+int sinusX   = 0;
+int vorigeY  = 0;
+float hoek   = 0.0;
+
+// UART buffer
+bool receiving = false;
+uint8_t buffer[sizeof(InputData)];
+uint8_t index = 0;
+
+void setup() 
+{
+  Serial.begin(9600);
+  mySerial.begin(9600);
+
+  uint16_t ID = tft.readID();
+  Serial.print("Scherm ID: 0x");
+  Serial.println(ID, HEX);
+  
+  if (ID == 0xD3D3) ID = 0x9486;
+  tft.begin(ID);
+  tft.setRotation(1);
+  tft.fillScreen(ZWART);
+
+  tekenGrafiekOutline();
+  tekenRechterkant();
+
+  // Startpositie sinus
+  sinusX  = GRAFIEK_X + 1;
+  vorigeY = GRAFIEK_Y + (GRAFIEK_H / 2);
+}
+
+void loop() 
+{
+  // Potmeter uitlezen en blokken updaten
+  //potWaarde = map(analogRead(POTMETER_PIN), 0, 1023, 0, 100);
+  ontvangData();
+  updateBlokken();
+
+  // Sinus berekenen
+  int middenY   = GRAFIEK_Y + (GRAFIEK_H / 2);
+  //int amplitude = (GRAFIEK_H / 2) - 70;
+  int amplitude = map(data.PRESSURE_RAW_DATA, 0, 4095, 0, (GRAFIEK_H / 2) - 5);
+  int nieuweY = middenY - (int)(sin(hoek) * amplitude);
+
+  // Lijn tekenen
+  tft.drawLine(sinusX - 1, vorigeY, sinusX, nieuweY, ROOD);
+
+  vorigeY = nieuweY;
+  hoek   += 0.15;
+  sinusX++;
+
+  // Reset als einde grafiek bereikt
+  if (sinusX > GRAFIEK_X + GRAFIEK_B - 1) 
+  {
+    sinusX  = GRAFIEK_X + 1;
+    hoek    = 0.0;
+    vorigeY = middenY;
+
+    tft.fillRect(GRAFIEK_X + 1, GRAFIEK_Y + 1, GRAFIEK_B - 2, GRAFIEK_H - 2, LICHTROZE);
+    tekenRaster();
+  }
+  //delay(100);
+}
+
+// UART data lezen
+void ontvangData()
+{
+  while (mySerial.available())
+  {
+    uint8_t b = mySerial.read();
+    if (!receiving)
+    {
+      if (b == START_BYTE)
+      {
+        receiving = true;
+        index = 0;
+      }
+    }
+    else
+    {
+      buffer[index++] = b;
+      if (index >= sizeof(InputData))
+      {
+        memcpy(&data, buffer, sizeof(InputData));
+        receiving = false;
+        //Serial.println(data.HEARTBEAT_RAW_DATA); //debug
+      }
+    }
+  }
+}
+
+// Update blokken
+void updateBlokken() 
+{
+  // Blok 1 - Hartslag
+  tft.fillRect(191, 38, 75, 12, ZWART);
+  tft.setTextColor(LICHTROZE);
+  tft.setTextSize(2);
+  tft.setCursor(194, 40);
+  tft.print(data.HEARTBEAT_RAW_DATA);
+  tft.print(" BPM");
+
+  // Blok 2 - Saturatie
+  tft.fillRect(191, 120, 75, 12, ZWART);
+  tft.setTextColor(LICHTROZE);
+  tft.setCursor(194, 115);
+  tft.print(data.SATURATION_RAW_DATA);
+  tft.print(" %");
+
+  // Blok 3 - Temperatuur
+  tft.fillRect(191, 193, 75, 12, ZWART);
+  tft.setTextColor(LICHTROZE);
+  tft.setCursor(194, 190);
+  tft.print(data.NTC_RAW_DATA);
+  tft.print(" C");
+}
+
+// Raster
+void tekenRaster() 
+{
+  for (int i = 1; i < 5; i++) 
+  {
+    int y = GRAFIEK_Y + (GRAFIEK_H / 5) * i;
+    tft.drawFastHLine(GRAFIEK_X, y, GRAFIEK_B, GRIJS);
+  }
+  for (int i = 1; i < 4; i++) 
+  {
+    int x = GRAFIEK_X + (GRAFIEK_B / 4) * i;
+    tft.drawFastVLine(x, GRAFIEK_Y, GRAFIEK_H, GRIJS);
+  }
+}
+
+// Grafiek
+void tekenGrafiekOutline() 
+{
+  tft.fillRect(0, 0, 182, SCHERM_H, LICHTROZE);
+  
+  tekenRaster();
+
+  // Grafiek kader
+  tft.drawRect(GRAFIEK_X, GRAFIEK_Y, GRAFIEK_B, GRAFIEK_H, ZWART);
+
+  // Y-as labels
+  tft.setTextColor(ZWART);
+  tft.setTextSize(1);
+  for (int i = 0; i <= 5; i++) 
+  {
+    int y = GRAFIEK_Y + (GRAFIEK_H / 5) * i;
+    int waarde = 100 - (i * 20);
+    tft.setCursor(10, y - 3);
+    if (waarde < 100) tft.print(" ");
+    tft.print(waarde);
+  }
+
+  // X-as labels
+  tft.setCursor(GRAFIEK_X, GRAFIEK_Y + GRAFIEK_H + 5);
+  tft.print("0");
+  tft.setCursor(GRAFIEK_X + GRAFIEK_B - 6, GRAFIEK_Y + GRAFIEK_H + 5);
+  tft.print("t");
+
+  // Titel
+  tft.setTextColor(ROZE);
+  tft.setCursor(GRAFIEK_X + 45, 8);
+  tft.print("GRAFIEK");
+
+  // Scheidingslijn
+  tft.drawFastVLine(183, 0, SCHERM_H, BLAUW);
+}
+
+// Rechterkant
+void tekenRechterkant() 
+{
+  tft.setTextColor(ROZE);
+  tft.setTextSize(1);
+  tft.setCursor(193, 8);
+  tft.print("EXTRA INFO -");
+
+  tft.drawRect(190, 20, 90, 50, GRIJS);
+  tft.drawRect(190, 95, 90, 50, GRIJS);
+  tft.drawRect(190, 170, 90, 50, GRIJS);
+
+  tft.setTextColor(ROZE);
+  tft.setCursor(193, 25); tft.print("Hartslag:");
+  tft.setCursor(193, 100); tft.print("Saturatie:");
+  tft.setCursor(193, 175); tft.print("Temperatuur:");
+}
